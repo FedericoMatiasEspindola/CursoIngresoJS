a6dfb09884e9e0b66e5ff69e69bd06242a64905f
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-06 12:32:43 -0300

    ejercicios 3,4,5

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,20 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+	/*var nombre;
+	nombre= prompt("nombre");
+	document.getElementById('elNombre').value;
+	var edad;
+	edad= prompt("edad");
+	document.getElementById('laEdad').value;
+	alert("su nombre es:" +nombre "su edad es:" +edad);*/
+
+	var nombre;
+	var edad;
+
+	nombre= document.getElementById('elNombre').value;
+	edad= document.getElementById('laEdad').value;
+
+	alert("Usted se llama "+nombre +" y tiene" +edad + "años");
 }
 
