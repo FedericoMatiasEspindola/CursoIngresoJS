1a9d701b86447d44b790c27819eb202b80025e76
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-06 16:43:49 -0300

    parcial 1 ejercicio 2
    falta corregir la suma

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,12 @@
 function Mostrar()
 {
+  var importefinal;
+  var importeproducto;
+
+  importefinal= document.getElementById('importeFinal').value;
   
+  importeproducto= importefinal *21/100;
+  importeproducto= importefinal+importeproducto;
+
+  alert("El importe final es: " +importeproducto);
 }
