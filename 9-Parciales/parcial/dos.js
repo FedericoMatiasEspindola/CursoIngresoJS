98ec0b57fd2dc150510427cf3772088638fa63ad
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-06 16:51:04 -0300

    Add files via upload

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -5,7 +5,7 @@ function Mostrar()
 
   //importeproducto= prompt("ingrese el importe de su producto");
 
-  importefinal= document.getElementById('importeFinal').value=importeproducto;
+  importefinal= document.getElementById('importeFinal').value; //=importeproducto;
   
   importeproducto= importefinal *21/100;
   importeproducto= importefinal+importeproducto;
