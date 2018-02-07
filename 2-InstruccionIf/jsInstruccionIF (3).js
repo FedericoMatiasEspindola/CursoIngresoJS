7654bb97bdb4620ec9dc9eceee18cdf64b9153eb
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-07 12:18:26 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad
+	var edad;
+
+	edad= document.getElementById('edad').value;
+
+	if(edad<17)
+		{
+			alert("usted es menor de edad");
+
+		}
+
+	else
+		{
+			alert("usted es mayor de edad");
+
+		}
 
 
 }//FIN DE LA FUNCIÓN
