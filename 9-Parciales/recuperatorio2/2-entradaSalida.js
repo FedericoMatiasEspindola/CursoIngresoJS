00a763244800cc59bea93e2126d6a9dee0e04b00
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-07 02:33:45 -0300

    parcial 4 ejercicio 2
    arreglar

--- 9-Parciales/recuperatorio2/2-entradaSalida.js
+++ 9-Parciales/recuperatorio2/2-entradaSalida.js
@@ -1,7 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
-	
-}
+	//alert("Funciona 2-EntradaSalida");
+	 var importe;
+	 var importefinal;
+
+	 importe= prompt("ingrese el importe de su producto: ");
+
+	 importefinal= document.getElementById('importe').value=importe;
+      
+     importe= importefinal/100*21;
+     importe= importefinal+importe;
+
+     alert("su importe final es: " + importe);
+}    
+
 
