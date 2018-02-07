18d435e1d0f338c0ac9f9f91ebe532e404043bc4
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-07 16:36:49 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,32 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
 
+	edad= document.getElementById('edad').value;
+
+	if(edad <13)
+		{
+			
+			alert("es niño");
+
+		}
+
+	  	else
+			
+			if(edad >17)
+				{
+					
+					alert("es mayor de edad");
+
+				}
+
+				else
+					{
+
+						alert("es adolescente");
+
+					}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,33 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+	var estadoCivil;
+
+	edad= document.getElementById('edad').value;
+	estadoCivil= document.getElementById('estadoCivil').value;
+
+	if(edad >18)
+
+		if(estadoCivil == "Soltero")
+			{
+
+				alert("Es soltero y no es menor");
+
+			}
+
+
+
+
+
+	/*if(edad >18 && estadoCivil == "Soltero")
+		{
+
+			alert("Es soltero y no es menor");
+
+		}*/
+
+
 
 
 }//FIN DE LA FUNCIÓN
