0ae99d9f4c8c99b4cc5f5040f2995e536676371d
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-07 17:04:25 -0300

    Add files via upload

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
 
+	var largo;
+	var ancho;
+	var perimetro;
+	var perimetroAlambre;
+
+	largo= document.getElementById('alrgo').value;
+	ancho= document.getElementById('ancho').value;
+
+	perimetro= largo*ancho
+	perimetroAlambre= perimetro*3
+
+	alert("se necesita: " + perimetroAlambre + " metros de alambre");
+
+
+
 }
