d37505ca7f196dd204320ff2510a58ccf9b5b72b
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-07 10:52:49 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+	var resultado;
+
+	importe= document.getElementById('importe').value;
+
+	importe= parseInt(importe);
+
+	resultado= importe*25/100;
+	resultado= importe - resultado;
+
+	document.getElementById('resultado').value=resultado;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var resultado;
 
+	sueldo= document.getElementById('sueldo').value;
+
+	sueldo= parseInt(sueldo);
+	
+	resultado= sueldo*10/100;
+	resultado= sueldo+resultado;
+	
+	document.getElementById('resultado').value=resultado;
+	
+	
 	
 }
