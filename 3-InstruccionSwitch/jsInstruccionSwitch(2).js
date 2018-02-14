adff9267bce64fa95647d21de682c8aa825b5da2
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-14 11:54:37 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,53 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño;
 
-alert (mesDelAño);
+	mesDelAño = document.getElementById('mes').value;
+
+	switch(mesDelAño)
+	{
+		case "Enero":	
+		case "Febrero":	
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno.");
+
+			break;
+
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace frio.");
+
+			break;
+
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+			alert("Ya pasamos el frio, ahora calor!!!.");
+
+
+
+
+
+
+
+
+
+
+	}
 
 
 
 
 }//FIN DE LA FUNCIÓN
+
+/*
+primavera: septiembre
+·verano: diciembre
+·otoño: marzo
+·Invierno: mayo
+*/
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,10 +1,25 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
+	var mesDelAño 
 	
+	mesDelAño= document.getElementById('mes').value;
+
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			alert("Este mes no tiene más de 29 días.");
+
+			break;
+
+		default:
+			alert("Este mes tiene 30 o más días");
+
+
+
+	}
+
+
 	
 
 
