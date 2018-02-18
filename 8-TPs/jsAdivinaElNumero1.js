504b2ff48e6eb3c54142a0007a7195757bc5aa85
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-17 22:39:06 -0300

    Add files via upload
    arreglar intentos

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,55 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
+	var contadorIntentos;
+	var numeroSecreto; 
+
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
 	
 
+	numeroSecreto=Math.floor(Math.random() * 100) + 1;	
+	console.log(numeroSecreto);
+	
+	//contadorIntentos= 0;
+	//document.getElementById('intentos').value= intentos + contadorIntentos;
 }
 
 function verificar()
 {
+
+
+	var intentos;
+	var numero;
+
+	numero=document.getElementById('numero').value;
 	
+	intentos= intentos+1;
+
+	document.getElementById('intentos').value= intentos;
+	
+
+	if(numero== numeroSecreto)
+	{
+		alert("Usted es un ganador!!! y en solo " + intentos + " intentos");
+	}
+				
+	if(numero< numeroSecreto)
+	{
+		alert("falta…");
+	}
+				
+	if(numero> numeroSecreto)	
+	{	
+		alert("se pasó…");
+	}
+			
+	
+
 	
 }
+
+//Genero el número RANDOM entre 1 y 100
+	 
+		//alert(numeroSecreto );
