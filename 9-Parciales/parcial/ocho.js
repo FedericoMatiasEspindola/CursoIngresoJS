ee488f05fc8c13e2de4294735d3cba09ffd74e44
FedericoMatiasEspindola <36193713+FedericoMatiasEspindola@users.noreply.github.com>
2018-02-24 15:54:47 -0300

    Add files via upload

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,17 +1,17 @@
 /*
 	ingreso de alumnos (no sabemos cuantos hay), pedir nombres
 	decir cuantos alumnos ingrese,
-	1-cantidad alumnos
-	2-promedio total
-	3-cantidad de mujeres(4)
-	4-cantidad de varones(5)
-	5-cantidad de varones desaprovados(1)
+	1-cantidad alumnos 	-
+	2-promedio total	-
+	3-cantidad de mujeres(4)	-
+	4-cantidad de varones(5)	-
+	5-cantidad de varones desaprovados(1)	-
 	6-nombre del alumno con mayor nota
 	7-promedio de las mujeres
 	8-sexo de menor de edad
 	9-promedio de las edades de los aprobados (177/6)
-	10-cantidad de notas pares(4)
-	11-cantidad de notas impares(3)
+	10-cantidad de notas pares(4)	-
+	11-cantidad de notas impares(3)	-
 */
 
 function Mostrar()
@@ -25,10 +25,15 @@ function Mostrar()
 	var sexo;
 	var acumuladorF=0;
 	var acumuladorM=0;
-	var maximo;
-	var minimo;
-	var nombreMaximo;
-	var promedioMujeres;
+	//var maximo;
+	//var minimo;
+	//var nombreMaximo;
+	//var promedioMujeres;
+	var varonesdesaprobados=0;
+	var edad;
+	var acumuladorEdad=0;
+	var contadorDePar=0;
+	var contadorDeImpar=0;
 
 	
 	
@@ -36,12 +41,12 @@ function Mostrar()
 	
 	while(respuesta!="no")
 	{
-		
+		//cantidad de alumnos
 		alumnos=prompt("ingrese nombre del alumno");
 		contador= contador+1;
 		respuesta=prompt("Si para continuar , No para salir");
 	
-		
+		//sexo F o M
 		sexo=prompt("ingrese f ó m ");
 		respuesta=prompt("Si para continuar , No para salir");
 		while(sexo!= "f" && sexo!= "m")
@@ -58,10 +63,9 @@ function Mostrar()
 			acumuladorM++;
 		}
 		
-		
 
-		
-
+	
+		//notas de alumnos
 		nota=prompt("ingrese su nota");
 		nota=parseInt(nota);
 		acumulador=acumulador+nota;
@@ -72,20 +76,52 @@ function Mostrar()
 			nota=parseInt(nota);
 			respuesta=prompt("Si para continuar , No para salir");
 		}
+
+		//varones desaprobados
+		if(nota<4 && sexo=="m")
+		{
+			varonesdesaprobados++;
+		}
+
+
 		
+		//edad pedida
+		if(edad <0 && edad >100 )
+		{
+			edad=prompt("ingrese su edad");
+			edad=parseInt(edad);
+			respuesta=prompt("Si para continuar , No para salir");
+		}
+
+
+
+		//notas pares e impares
+		if(nota%2==0)
+		{
+			contadorDePar++;
+		}
+			else
+			{
+				contadorDeImpar++;
+			}
+
+
 
 
 	}
 	
 	promedio=acumulador/contador;
+	//promedioMujeres=
 
 
 	document.write("<br> son " +contador+ " de alumnos");
 	document.write("<br> el promedio de las nota son "+promedio);
 	document.write("<br> mujeres: "+acumuladorF+" varones: "+acumuladorM);
 	//document.write("<br> nota del alumno con mas nota "+maximo);
-
-
+	document.write("<br> varones desaprobados "+varonesdesaprobados);
+	//document.write("<br> el promedio de las nota son "+promedioMujeres);
+	document.write("<br> notas pares ")+contadorDePar;
+	document.write("<br> notas impares ")+contadorDeImpar;
 
 }
 
